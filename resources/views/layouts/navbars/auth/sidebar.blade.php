f591ed0d25d5f62c620caa76c06cea9424ad15e3
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-01-22 16:54:50 +0300

    first commit

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -4,7 +4,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="{{ route('dashboard') }}">
-            <img src="{{ asset('assets/img/vfu_logo.png') }}" alt="vfu_logo" width="80%" height="80%"
+            <img src="{{ asset('assets/img/vfu_logo.png') }}" alt="vfu_logo" width="80%" height="100%"
                 class="img-fluid">
         </a>
     </div>
